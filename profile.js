05a450c5c31563927f7ede3ecb9f56074829c40a
MagusDrk <34247282+MagusDrk@users.noreply.github.com>
2017-12-04 15:32:43 -0500

    First P5js version
    All files required to profle based on p5js sketch

---
+++ profile.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 500);
+  
+}
+
+function draw() {
+  background(255);
+  fill(int(random(50, 250)), 0, 0);
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  text("Hello World", width/2, height/2);
+}
