faa1656d5e74bec7aba2868c41d1c4e14e23959e
MagusDrk <34247282+MagusDrk@users.noreply.github.com>
2017-12-13 16:51:57 -0500

    Cambio pequeño
    Nada importante

--- profile.js
+++ profile.js
@@ -8,5 +8,5 @@ function draw() {
   fill(int(random(50, 250)), 0, 0);
   textSize(30);
   textAlign(CENTER, CENTER);
-  text("Hello World", width/2, height/2);
+  text("Profile under construction", width/2, height/2);
 }
