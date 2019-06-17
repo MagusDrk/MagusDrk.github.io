804ff350402dcb30916f8c6aa2df94ffdb4e736a
MagusDrk <magus.drk@gmail.com>
2019-06-17 09:22:24 -0500

    nueva version 0.0
    Nueva versión de inicio para validar apartura con processing

---
+++ Magus_Teacher/Magus_Teacher.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+
+function draw() {
+  background(255, 255, 200);
+  textAlign(CENTER, CENTER);
+  text("Under construction", width/2, height/2);
+}
